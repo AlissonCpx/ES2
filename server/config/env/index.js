92d0fe7a593567dcb7d914674b0d7c04f2730501
Alisson Andrade <alissondeandrade1@gmail.com>
2020-06-02 00:24:30 -0300

    install heroku

--- server/config/env/index.js
+++ server/config/env/index.js
@@ -6,7 +6,7 @@ module.exports = {
     dataConfig: {
       MYSQL: {
         database: 'kudgv9t2q6almxt8',
-        host: 'pqxt96p7ysz6rn1f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	',
+        host: 'pqxt96p7ysz6rn1f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
         port: '3306',
         user: 'xciewfnknqxhyd4o',
         password: 'dcm9l3jx04xua5g4'
