9a464cfda4864c1b4d89d5948160582657ed24df
Alisson Andrade <alissondeandrade1@gmail.com>
2020-06-02 00:18:41 -0300

    config banco heroku

--- server/config/env/index.js
+++ server/config/env/index.js
@@ -5,11 +5,11 @@ module.exports = {
     portApi: 3303,
     dataConfig: {
       MYSQL: {
-        database: 'mydb',
-        host: 'localhost',
+        database: 'kudgv9t2q6almxt8',
+        host: 'pqxt96p7ysz6rn1f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	',
         port: '3306',
-        user: 'root',
-        password: '1234'
+        user: 'xciewfnknqxhyd4o',
+        password: 'dcm9l3jx04xua5g4'
       }
     }
   };
