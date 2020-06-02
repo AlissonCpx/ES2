8f7d6ce178f5c772650972a91f30ca2a2d2dc84c
Alisson Andrade <alissondeandrade1@gmail.com>
2020-06-02 00:10:42 -0300

    Enviando api para trabalho

---
+++ server/config/env/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+    env: 'development',
+    nomeApi: 'helloworld',
+    urlApi: 'http://localhost',
+    portApi: 3303,
+    dataConfig: {
+      MYSQL: {
+        database: 'mydb',
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
+        password: '1234'
+      }
+    }
+  };
